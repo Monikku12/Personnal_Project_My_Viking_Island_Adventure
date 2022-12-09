b7e9d2863a86f37fb00362b3d8a5bd82e335bd81
Monikku12 <monique.paymentboulanger@gmail.com>
2022-12-10 00:28:29 +0900

    adding fake logo

--- src/components/title.js
+++ src/components/title.js
@@ -1,11 +1,17 @@
 import * as React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, Image, StyleSheet } from "react-native";
+
+const logo = require("../../images/logo/title_logo.webp");
 
 function Title({ navigation }) {
     return (
         <View
             style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
+            <Image
+                style={styles.logo}
+                source={logo}
+            />
             <Text>My Dragons Island Adventure</Text>
             <Button
                 title="English"
@@ -18,5 +24,15 @@ function Title({ navigation }) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 50,
+    },
+    logo: {
+        width: 66,
+        height: 58,
+    },
+});
 
 export default Title;
