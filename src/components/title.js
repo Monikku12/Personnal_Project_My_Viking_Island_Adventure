29a1e6e5d46dc3e7fc4a875f94c14ab03fe80621
Monikku12 <monique.paymentboulanger@gmail.com>
2022-12-11 23:07:52 +0900

    title fake background and logo added. Fixing buttons now.

--- src/components/title.js
+++ src/components/title.js
@@ -1,37 +1,69 @@
 import * as React from "react";
-import { Button, View, Text, Image, StyleSheet } from "react-native";
+import {
+    Button,
+    View,
+    Text,
+    Image,
+    ImageBackground,
+    StyleSheet,
+} from "react-native";
 
-const logo = require("../../images/logo/title_logo.webp");
+const logo = require("../images/logo/title_logo.png");
+const backgroundImage = require("../images/background/title_background.png");
 
 function Title({ navigation }) {
     return (
-        <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
-            <Image
-                style={styles.logo}
-                source={logo}
-            />
-            <Text>My Dragons Island Adventure</Text>
-            <Button
-                title="English"
-                onPress={() => navigation.navigate("BedroomEn")}
-            />
-            <Button
-                title="Français"
-                onPress={() => navigation.navigate("BedroomFr")}
-            />
+        <View style={styles.container}>
+            <ImageBackground
+                source={backgroundImage}
+                style={styles.imageBackground}
+            >
+                <Image style={styles.logo} source={logo} />
+                <Text style={styles.text}>My Dragons Island Adventure</Text>
+                <Button
+                    style={styles.button}
+                    title="English"
+                    onPress={() => navigation.navigate("BedroomEn")}
+                />
+                <Button
+                    style={styles.button}
+                    title="Français"
+                    onPress={() => navigation.navigate("BedroomFr")}
+                />
+            </ImageBackground>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 50,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
     },
     logo: {
-        width: 66,
-        height: 58,
+        flex: 1,
+        width: "25%",
+        height: "50%",
+        resizeMode: "center",
+        alignSelf: "center",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    imageBackground: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+    },
+    text: {
+        color: "black",
+        fontSize: 12,
+        lineHeight: 15,
+        fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "white",
     },
 });
 
